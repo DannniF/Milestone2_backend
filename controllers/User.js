37b99ae79e36bd0c3fa7c7ec8afadd8319cb508b
Eyrma <erotstein@outlook,com>
2024-02-20 18:33:02 -0800

    updating the user function

--- controllers/User.js
+++ controllers/User.js
@@ -1,32 +1,60 @@
-const bcrpyt = require('bcrypt')
-const User = require('../models/User')
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const app = require('express');
+const jwt = require('jsonwebtoken');
 
-async function signUp (req, res) {
+const JWT_SECRET = "asdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm"
 
-    try {
-        const { email, password } = req.body
-        const encryptedPassword = await bcrpyt.hash(password, 10)
-        const user = await new User({
-            email,
-            password: encryptedPassword
-        })
-        user.save()
-        res.status(200).json({message: 'sign up error'})
-        // user.save(function (err, data) {
-        //     // if (err) {
-        //     //     console.log('sign up error', error)
-        //     //     res.status(500).json({message: 'sign up error'})
-        //     // } else {
-        //     //     res.status(200)
-        //     // }
-        // })
-    } catch (error) {
-        console.log('sign up error', error)
-        res.status(500).json({message: 'sign up error'})
-    }
-
+async function signUp(req, res) {
+  const { name: { firstName, lastName }, email, password } = req.body;
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ 
+    name: { firstName, lastName }, 
+    email, 
+    password: encryptedPassword 
+  });
+  
+  try {
+    await user.save();
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
 }
 
+async function login (req, res) { 
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(500).json({ error: 'User not found' });
+  }
+  //check if password is equal to user password
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({},JWT_SECRET);
+
+    return res.json({status: "ok", data: token});
+  } else {
+    return res.status(500).json({ status: 'error', error: 'Invalid user name or password' });
+  }
+
+}
+// async function signUp (req, res) {
+//     try {
+//       const { name: { first, last }, email, password } = req.body;
+//       const encryptedPassword = await bcrypt.hash(password, 10);
+//       const user = await new User({
+//         name: { first, last },
+//         email,
+//         password: encryptedPassword,
+//       }).save();
+//       res.status(201).json({ message: 'User registered successfully', user });
+//     } catch (error) {
+//       console.error('Error registering user:', error);
+//       res.status(500).json({ error: 'An error occurred while registering the user' });
+//     }
+//   });
+
 module.exports = {
-    signUp,
+  signUp,
+  login,
 }
--- models/User.js
+++ models/User.js
@@ -2,6 +2,11 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
+    name: {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        fullName: { type: String, set: (names) => names.firstName + ' ' + names.lastName }
+    },
     email: {
         type: String,
         required: true,
@@ -11,6 +16,13 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-})
+    role: {
+        type: String,
+        enum: ['user', 'admin'], default: 'user'
+    },
+    createdAt: {
+        type: Date, default: Date.now
+    },
+});
 
 module.exports = mongoose.model('User',UserSchema)
